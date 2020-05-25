95cdebe74dfbc3715f61005193607c1cdf9d8d6a
BuildTools <unconfigured@null.spigotmc.org>
2020-05-25 14:36:29 +0200

    Fixed color esp

--- Yakuza/options.h
+++ Yakuza/options.h
@@ -25,8 +25,8 @@ namespace options
 		inline bool health = false;
 		inline bool name = false;
 		inline bool rainbow = false;
-		inline float box_width = 2.f;
-		static inline float color[3] = { 255.0f, 0.0f, 0.0f };
+		inline float box_width = 2.f;			
+	 inline float color[3] = { 255.0f, 0.0f, 0.0f };
 		inline float rainbowcolor[3] = { 255.f, 0.f, 0.f };
 	}
 	inline bool freeze = false;
